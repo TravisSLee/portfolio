60114a65f0cacb384d8b1f7d582d14e49c61d7c4
Travis Lee <travisslee223@gmail.com>
2024-04-18 17:30:47 -0400

    improve local env

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 import Projects from "./components/Projects";
 
+
 function App() {
   return (
     <div className="App">
-
       <About />
       <Projects />
       <Footer />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Link } from 'react-bootstrap'
+import Nav from 'react-bootstrap/Nav'
 
 export default function Projects() {
 
 
+  // return (
+  //   <Link href="https://github.com/TravisSLee/My_Tunes">My_Tunes</Link>
+  // )
   return (
-    <Link href="https://github.com/TravisSLee/My_Tunes">My_Tunes</Link>
-  )
+    <Nav defaultActiveKey="/home" className="flex-column">
+      <Nav.Link href="https://github.com/TravisSLee/My_Tunes">My_Tunes</Nav.Link>
+      {/* <Nav.Link eventKey="link-2">Link</Nav.Link>
+      <Nav.Link eventKey="disabled" disabled>
+        Disabled
+      </Nav.Link> */}
+    </Nav>
+  )  
 }
